6c46138a41adeca8c8a3ef7957af093862c164dc
root <root@US-SGHOSH.fortinet-us.com>
2021-06-15 00:11:53 -0700

    navigation/ using antD for the main navigation

--- src/App.js
+++ src/App.js
@@ -2,43 +2,95 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css'
 
 import AddTutorial from "./components/AddTutorial";
 import Tutorial from "./components/Tutorial";
 import TutorialsList from "./components/TutorialsList";
-import {Login} from "./components/Login";
+import { Login } from "./components/Login";
 
-function App() {
-  return (
-    <Router>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/tutorials" className="navbar-brand">
-          Tutorial " /tutorial "
-        </a>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/tutorials"} className="nav-link">
-              Tutorials
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/add"} className="nav-link">
-              Add
-            </Link>
-          </li>
+const { SubMenu } = Menu;
+
+class App extends React.Component {
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({ current: e.key });
+  };
+  render() {
+    const { current } = this.state;
+    return (
+      <Router>
+        <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal" className="MainNavigationMenuClass" 
+        >
+          <Menu.Item key="mail" icon={<MailOutlined />}>
+            Navigation One
+          </Menu.Item>
+
+          <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
+            <Menu.ItemGroup title="Item 1">
+              <Menu.Item key="setting:1">Option 1</Menu.Item>
+              <Menu.Item key="setting:2">Option 2</Menu.Item>
+            </Menu.ItemGroup>
+            <Menu.ItemGroup title="Item 2">
+              <Menu.Item key="setting:3">Option 3</Menu.Item>
+              <Menu.Item key="setting:4">Option 4</Menu.Item>
+            </Menu.ItemGroup>
+          </SubMenu>
+        </Menu>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/add" component={AddTutorial} />
+            <Route path="/tutorials/:id" component={Tutorial} />
+          </Switch>
         </div>
-      </nav>
 
-      <div className="container mt-3">
-        <Switch>
-          <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/add" component={AddTutorial} />
-          <Route path="/tutorials/:id" component={Tutorial} />
-        </Switch>
-      </div>
-    </Router>
-  );
+      </Router>
+    );
+  }
 }
+
+// ReactDOM.render(<App />, mountNode);
+
+// function App() {
+//   return (
+//     <Router>
+//       <nav className="navbar navbar-expand navbar-dark bg-dark">
+//         <a href="/tutorials" className="navbar-brand">
+//           Tutorial " /tutorial "
+//         </a>
+//         <div className="navbar-nav mr-auto">
+//           <li className="nav-item">
+//             <Link to={"/tutorials"} className="nav-link">
+//               Tutorials
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link to={"/add"} className="nav-link">
+//               Add
+//             </Link> 
+//           </li>
+//         </div>
+//       </nav>
+
+//       <div className="container mt-3">
+//         <Switch>
+//           <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+//           <Route exact path="/login" component={Login} />
+//           <Route exact path="/add" component={AddTutorial} />
+//           <Route path="/tutorials/:id" component={Tutorial} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
 
 export default App;
